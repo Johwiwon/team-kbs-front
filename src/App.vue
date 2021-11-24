77194f926be0b228197a3b0dba07e31e9c485a51
Johwiwon <hwiba0@gmail.com>
2021-11-24 22:26:37 +0900

    레이아웃
    레이아웃

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div>
-        <h1>디아-득템</h1>
-      </div>
-    </v-app-bar>
+<v-app>
+  <v-navigation-drawer app>
+    <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            영상관리
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            관리자페이지
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-main>
-      <!-- router/index.js 기준으로 여기가 바뀐다. -->
-      <router-view />
-    </v-main>
-  </v-app>
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
 
-  data: () => ({
-    //
-  }),
-
-  created() {},
+  data () {
+      return {
+        items: [
+          { title: '영화', icon: 'mdi-view-dashboard' ,to:"/home"},
+          { title: '애니', icon: 'mdi-image' ,to:"/ab"},
+          { title: '재생확인', icon: 'mdi-help-box' ,to:"/showVideo"},
+        ],
+        right: null,
+      }
+    },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import about from "@/views/About.vue";
+import showVideo from "@/views/showVideo.vue";
 
 Vue.use(VueRouter);
 
@@ -17,13 +19,20 @@ const routes = [
       import(/* webpackChunkName: "exmaple" */ "../views/Example.vue"),
   },
   {
-    path: "/",
+    path: "/ab",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: about,
+  },
+  {
+    path: "/showVideo",
+    name: "showVideo",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: showVideo,
   },
 ];
 
